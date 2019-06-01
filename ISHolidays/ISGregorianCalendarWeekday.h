f4f931efed334c0be59158d361d8964f00bc7339
Guillaume Algis <guillaume.algis@ipsosante.fr>
2019-06-02 01:52:19 +0200

    Add enum listing the weekdays in the gregorian calendar

---
+++ ISHolidays/ISGregorianCalendarWeekday.h
@@ -0,0 +1,14 @@
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSInteger, ISGregorianCalendarWeekday) {
+    ISGregorianCalendarWeekdaySunday = 1,
+    ISGregorianCalendarWeekdayMonday,
+    ISGregorianCalendarWeekdayTuesday,
+    ISGregorianCalendarWeekdayWednesday,
+    ISGregorianCalendarWeekdayThursday,
+    ISGregorianCalendarWeekdayFriday,
+    ISGregorianCalendarWeekdaySaturday
+};
+NS_ASSUME_NONNULL_END
