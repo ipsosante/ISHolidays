cef550c871d2e6a8b315258741f8d6893ffd3cbb
Guillaume Algis <guillaume.algis@ipsosante.fr>
2019-06-01 19:49:29 +0200

    Add tests for isEqualToDateExcludingTime:

---
+++ ISHolidaysTests/NSDate+CompareTests.m
@@ -0,0 +1,117 @@
+
+#import <XCTest/XCTest.h>
+
+#import "NSDate+Compare.h"
+
+@interface NSDateCompareTests : XCTestCase
+
+@property (strong, nonatomic) NSISO8601DateFormatter *isoDateFormatter;
+
+@end
+
+@implementation NSDateCompareTests
+
+- (void)setUp
+{
+    [super setUp];
+
+    self.isoDateFormatter = [[NSISO8601DateFormatter alloc] init];
+}
+
+- (void)testSameDateAndTime
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"1970-01-01T00:00:00+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"1970-01-01T00:00:00+00:00"];
+
+    XCTAssertTrue([left isEqualToDateExcludingTime:right]);
+}
+
+- (void)testSameDateDifferentHour
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"1970-01-01T00:00:00+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"1970-01-01T01:00:00+00:00"];
+
+    XCTAssertTrue([left isEqualToDateExcludingTime:right]);
+}
+
+- (void)testSameDateDifferentMinutes
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"1970-01-01T00:00:00+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"1970-01-01T00:01:00+00:00"];
+
+    XCTAssertTrue([left isEqualToDateExcludingTime:right]);
+}
+
+- (void)testSameDateDifferentSeconds
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"1970-01-01T00:00:00+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"1970-01-01T00:00:01+00:00"];
+
+    XCTAssertTrue([left isEqualToDateExcludingTime:right]);
+}
+
+- (void)testSameDateDifferentTimezone
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"1970-01-01T00:00:00+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"1970-01-01T00:00:00+01:00"];
+
+    XCTAssertTrue([left isEqualToDateExcludingTime:right]);
+}
+
+- (void)testSameDateDifferentTimeSameTimzone
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"1970-01-01T03:38:27+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"1970-01-01T11:42:52+00:00"];
+
+    XCTAssertTrue([left isEqualToDateExcludingTime:right]);
+}
+
+- (void)testSameDateDifferentTime
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"1970-01-01T12:33:32+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"1970-01-01T04:14:04+03:00"];
+
+    XCTAssertTrue([left isEqualToDateExcludingTime:right]);
+}
+
+- (void)testSameDateDifferentTimeExceedingMidnight
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"1970-01-01T12:33:32+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"1970-01-01T04:14:04+10:00"];
+
+    XCTAssertFalse([left isEqualToDateExcludingTime:right]);
+}
+
+- (void)testDifferentYearSameTime
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"2018-01-01T00:00:00+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"2009-01-01T00:00:00+00:00"];
+
+    XCTAssertFalse([left isEqualToDateExcludingTime:right]);
+}
+
+- (void)testDifferentMonthSameTime
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"2019-01-01T00:00:00+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"2019-04-01T00:00:00+00:00"];
+
+    XCTAssertFalse([left isEqualToDateExcludingTime:right]);
+}
+
+- (void)testDifferentDaySameTime
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"2019-01-01T00:00:00+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"2019-01-23T00:00:00+00:00"];
+
+    XCTAssertFalse([left isEqualToDateExcludingTime:right]);
+}
+
+- (void)testDifferentDateAndTime
+{
+    NSDate *left = [self.isoDateFormatter dateFromString:@"2018-11-21T12:33:32+00:00"];
+    NSDate *right = [self.isoDateFormatter dateFromString:@"2009-08-04T04:14:04+02:00"];
+
+    XCTAssertFalse([left isEqualToDateExcludingTime:right]);
+}
+
+@end
